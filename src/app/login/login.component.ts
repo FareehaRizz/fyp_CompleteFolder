b3ecbef28a699af5e688ca557c7c7910fed525a6
maryam javed <maryamjavedshk@gmail.com>
2025-04-06 13:45:13 +0500

    backend connection

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,62 +1,76 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
-
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
- 
-  username: string = '';
-  password: string = '';
-  
+  email: string = "";
+  password: string = "";
+
   // Static credentials
   staticCredentials = {
-    username: 'admin',
-    password: 'password123'
+    email: "admin",
+    password: "password123",
   };
 
-  //loginData = { username: '', password: '' };
-  
   constructor(private router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
   // login() {
   //   const storedUser = localStorage.getItem('user');
   //   if (storedUser) {
   //     const user = JSON.parse(storedUser);
-  //     if (this.username === user.username && this.password === user.password) {
+  //     if (this.email === user.email && this.password === user.password) {
   //       localStorage.setItem('isLoggedIn', 'true'); // Store login state
   //       this.router.navigate(['/dashboard']); // Redirect to dashboard
   //     } else {
-  //       alert('Invalid username or password');
+  //       alert('Invalid email or password');
   //     }
   //   } else {
   //     alert('No user found. Please sign up first.');
   //   }
   // }
- 
 
   login() {
+    let loginData = { email: this.email, password: this.password };
+
     // Check if the entered credentials match the static credentials
-    if (this.username === this.staticCredentials.username && this.password === this.staticCredentials.password) {
-      localStorage.setItem('isLoggedIn', 'true'); // Store login state
-      this.router.navigate(['/dashboard']); // Redirect to dashboard
-    } else {
-      alert('Invalid username or password');
-    }
+    // if (
+    //   this.email === this.staticCredentials.email &&
+    //   this.password === this.staticCredentials.password
+    // ) {
+
+    //   localStorage.setItem("isLoggedIn", "true"); // Store login state
+    //   this.router.navigate(["/dashboard"]); // Redirect to dashboard
+    // } else {
+    //   alert("Invalid email or password");
+    // }
+    const api = fetch("http://localhost:5000/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(loginData),
+      credentials: "include",
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        if (data) {
+          this.router.navigate(["/dashboard"]);
+        }
+      })
+      .catch((err) => console.error("Login error:", err));
   }
 
- 
-  onSubmit(){
+  onSubmit() {
     this.login();
   }
- 
 }
-
-
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,34 +1,53 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  selector: "app-signup",
+  templateUrl: "./signup.component.html",
+  styleUrls: ["./signup.component.scss"],
 })
 export class SignupComponent implements OnInit {
+  fullname: string = "";
+  email: string = "";
+  password: string = "";
+  organization: string = "";
+  public_key: string = "";
+
   signupForm: FormGroup;
   hidePassword: boolean = true; // Default password visibility
 
-
   constructor(private fb: FormBuilder) {
     this.signupForm = this.fb.group({
-      username: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(6)]],
     });
-   }
-
-  ngOnInit(): void {
   }
+
+  ngOnInit(): void {}
   onSubmit() {
-    if (this.signupForm.valid) {
-      console.log('User registered:', this.signupForm.value);
-    }
+    let signupData = {
+      name: this.fullname,
+      email: this.email,
+      password: this.password,
+      organization: this.organization,
+      publicKey: this.public_key,
+    };
+    fetch("http://localhost:5000/auth/RegisterUser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(signupData),
+      credentials: "include",
+    }).then((res) => {
+      if (res.status == 200) {
+        return res.json();
+      }
+    });
   }
   // Toggle password visibility
   togglePasswordVisibility(input: HTMLInputElement) {
-    input.type = input.type === 'password' ? 'text' : 'password';
+    input.type = input.type === "password" ? "text" : "password";
   }
-
 }
