5080e23fdf72cbf143ca0892efda62c9f7c087ae
maryam javed <maryamjavedshk@gmail.com>
2025-04-24 21:48:24 +0500

    update user profile

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -15,7 +15,7 @@ export class UserComponent implements OnInit {
     city: "",
     country: "",
     postalCode: "",
-    about: "",
+    userAbout: "",
   };
 
   ngOnInit(): void {
@@ -25,7 +25,6 @@ export class UserComponent implements OnInit {
     })
       .then(async (res) => {
         const data = await res.json();
-        console.log(data);
         if (!res.ok) throw new Error(data.msg || "Failed to fetch profile");
         const name = data.name || "";
         const nameParts = name.trim().split(" ");
@@ -33,9 +32,45 @@ export class UserComponent implements OnInit {
         this.user.lastName = nameParts.slice(1).join(" ") || "";
         this.user.email = data.email;
         this.user.organization = data.organization;
+        this.user.address = data.address;
+        this.user.city = data.city;
+        this.user.country = data.country;
+        this.user.postalCode = data.postalCode;
+        this.user.userAbout = data.aboutUser;
       })
       .catch((err) => {
         console.error("Error fetching profile:", err);
+      });
+  }
+
+  updateProfile() {
+    const userData = {
+      name: `${this.user.firstName} ${this.user.lastName}`,
+      email: this.user.email,
+      organization: this.user.organization,
+      address: this.user.address,
+      city: this.user.city,
+      country: this.user.country,
+      postalCode: this.user.postalCode,
+      aboutUser: this.user.userAbout,
+    };
+    fetch("http://localhost:5000/auth/updateuserprofile", {
+      method: "PUT",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    })
+      .then(async (res) => {
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.msg || "Update failed");
+        }
+        console.log("Profile updated:", data);
+      })
+      .catch((err) => {
+        console.error("Update failed:", err);
       });
   }
 }
